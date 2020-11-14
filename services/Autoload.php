1ae81f41c0dc22055ff75f96018cdaebf1cfe49a
Artem <Gavrilov>
2020-11-14 21:46:52 +0500

    d

--- services/Autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\services;
-    class Autoload {
-        public function loadClass($className) {
-            $className = preg_replace('/^App*/', '', $className);
-            include dirname(__DIR__) . $className . '.php';
-        }
-    }
--- services/IDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace App\services;
-    interface IDB {
-        const ERROR_CONNECT = 500;
-        
-        public function getAll($sql);
-        public function getOne($sql, $params = []);
-        public function execute($sql, $params = []);
-    }
